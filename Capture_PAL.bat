fb93795cedb3df81ac32c372556fb27e92f900b9
Luke D <102872635+LukeDischiave@users.noreply.github.com>
2023-11-04 12:31:49 -0400

    Update Capture_PAL.bat

--- Capture_PAL.bat
+++ Capture_PAL.bat
@@ -1,3 +1,3 @@
-ffmpeg -f dshow -i video="Datapath VisionRGB-E1 Video 01":audio="Line in at rear panel (Blue) (Realtek(R) Audio)" -c:a aac -b:a 240k -c:v libx264 -crf 13 -preset slow -pix_fmt yuv444p output.mkv
+ffmpeg -f dshow -i video="Datapath VisionRGB-E1 Video 01":audio="Line In (Realtek(R) Audio)" -ar 48000 -c:a aac -b:a 240k -c:v libx264 -crf 13 -preset slow -pix_fmt yuv444p output.mkv
 
 REM Luke Dischiave
